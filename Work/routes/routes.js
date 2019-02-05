336f946fe3ae9383180c98f4790c4ede75339c99
Vlad <Vlad1slavIP74>
2019-02-05 21:15:01 +0200

    second

--- Work/routes/routes.js
+++ Work/routes/routes.js
@@ -1,61 +1,66 @@
-const mongoDB = "mongodb://vlad:78a78a98a@ds062178.mlab.com:62178/vlad"
-const mydb = require ("../database/mydb")
-const mongoose = require("mongoose");
+const mongoDB = 'mongodb://vlad:78a78a98a@ds062178.mlab.com:62178/vlad';
+const mydb = require('../database/mydb');
+const mongoose = require('mongoose');
 
 const router = app => {
-  app.get('/',(request, response) =>{
+  app.get('/', (request, response) => {
     response.send({
       message: 'Welcome to the Node.js Express REST API!'
-    })
-  })
+    });
+  });
 
   app.post('/new', (request, response) => {
-    console.log(request.body)
-
-    mongoose.connect(mongoDB,function (err) {
-        if(err) {
-          console.log(err);
-          return response.status(500).send("Can not connect to data base");
-        }
-        //let DataBase = request.body;
-          let DataBase = new mydb({
-          _id: new mongoose.Types.ObjectId(),
-          firstName: request.body.firstName,
-          lastName: request.body.lastName,
-          age: parseInt(request.body.age),
-          profession: request.body.profession,
-          skills: request.body.skills,
-          salary: parseInt(request.body.salary)
-        });
-        console.log("SAVED");
-        DataBase.save(function (err) {
-          if (err) return response.status(500);
-        })
-    })
-
-
-   response.status(200).send('OK')
-});
-
-app.put('/data/:id', (request, response) =>{
-  mongoose.connect(mongoDB, function (err) {
-    if (err) throw err;
-    console.log("PUT");
     console.log(request.body);
-    mydb.findByIdAndUpdate(request.params.id, {$set:request.body} ,function (err, user) {
-      if (err) return response.status(500);
-      response.status(200).send('OK')
-  })
-  })
-})
-app.delete('/del/:id', (request, response) => {
-  mongoose.connect(mongoDB, function (err) {
-    if (err) throw err;
-    mydb.findByIdAndRemove(request.params.id, (err, res)=>{
-      if(err) return response.status(500);
-      response.status(200).send('OK')
-    })
-  })
-})
-}
+
+    mongoose.connect(mongoDB, (err) => {
+      if (err) {
+        console.log(err);
+        return response.status(500).send('Can not connect to data base');
+      }
+      //let DataBase = request.body;
+      const DataBase = new mydb({
+        _id: new mongoose.Types.ObjectId(),
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        age: parseInt(request.body.age),
+        profession: request.body.profession,
+        skills: request.body.skills,
+        salary: parseInt(request.body.salary)
+      });
+      console.log('SAVED');
+      DataBase.save((err) => {
+        if (err) return response.status(500);
+      });
+    });
+
+
+    response.status(200).send('OK');
+  });
+
+  app.put('/data/:id', (request, response) => {
+    mongoose.connect(mongoDB, (err) => {
+      if (err) throw err;
+      console.log('PUT');
+      console.log(request.body);
+      mydb.findByIdAndUpdate(request.params.id, { $set: request.body }, (err, user) => {
+        if (err) return response.status(500);
+        response.status(200).send('OK');
+      });
+    });
+  });
+  app.delete('/del/:id', (request, response) => {
+    mongoose.connect(mongoDB, (err) => {
+      if (err) throw err;
+      mydb.findByIdAndRemove(request.params.id, (err, res) => {
+        if (err) return response.status(500);
+        response.status(200).send('OK');
+      });
+    });
+  });
+  app.use((err, req, res, next) => {
+   //console.log('Error');
+   res.status(500).send('Internal Server Error');
+ });
+
+};
 module.exports = router;
